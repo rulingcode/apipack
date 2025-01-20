0c34c0e4186802ef3f18ca212477dfe131edb6cd
rulingcode@gmail.com <rulingcode@gmail.com>
2025-01-20 21:04:56 +0330

    1

---
+++ lib/CoreApi.dart
@@ -0,0 +1,32 @@
+import 'package:apipack/api.dart';
+
+class CoreApi {
+  CoreApi({required this.host});
+
+  String? token;
+  String? userId;
+  final String host;
+  final Duration retryDelay = Duration(milliseconds: 100);
+  late StartApi start;
+
+  void initializeApi() {
+    start = StartApi(ApiClient(basePath: host));
+  }
+
+  Future<SendCodeOut?> registrationSendCode(SendCode p) async {
+    while (true) {
+      try {
+        p.sessionID = token;
+        p.userID = userId;
+        return await start.registrationSendCodePost(p);
+      } catch (e) {
+        _logRetryAttempt(e.toString());
+        await Future.delayed(retryDelay);
+      }
+    }
+  }
+
+  void _logRetryAttempt(String error) {
+    print('Error occurred: $error');
+  }
+}
